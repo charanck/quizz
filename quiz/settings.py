80fae5b4e7f8623a81d4436a4b469570e45e3aa6
charanck <charank804.com>
2021-07-04 18:16:17 +0530

    Fixed static dir issue

--- quiz/settings.py
+++ quiz/settings.py
@@ -141,6 +141,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
