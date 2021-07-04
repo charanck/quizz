fa0dec1eea81e38c96a39c8d279f179eeb13a2e9
charanck <charank804.com>
2021-07-04 18:44:58 +0530

    Disabled Admin Route

--- quiz/urls.py
+++ quiz/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('',include('controller.urls')),
     path('/',include('controller.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
